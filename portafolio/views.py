e5fa220ebe4a57c0876d929b0a7404ea23c16498
randy <randysley@gmail.com>
2020-08-23 14:02:02 -0300

    haciendo cambios por unos errores

--- portafolio/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import projecto
-
-# Create your views here.
-def portafolio(request):
-	projectos = projecto.objects.all()
-	return render(request,'portafolio/portafolio.html',{'projectos':projectos}) 
